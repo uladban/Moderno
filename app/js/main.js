76ec0ce0a5ecccfdde03eefbad406381a1a05e1e
UladzinirBankouski <vlad.banek@gmail.com>
2020-07-24 15:23:02 +0300

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
 $(function(){
     var mixer = mixitup('.products__inner-box');
+
+    $(".rate-star").rateYo({
+        starWidth: "12px",
+        rating: 4.5,
+        readOnly: true,
+        halfStar: true
+      });
+
+    $(".product__slider-inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 })
