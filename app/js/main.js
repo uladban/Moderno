c6ee8893bf5d22aeb7e5770dd554899cf6180e76
UladzinirBankouski <vlad.banek@gmail.com>
2020-08-12 10:04:30 +0300

    addded adaptive header, footer added product one, 404, about pages

--- app/js/main.js
+++ app/js/main.js
@@ -26,6 +26,14 @@ $(function(){
         prefix: "$",
         
     });
+    $('.product-one__tabs .tab').on('click', function(event) {
+        var id = $(this).attr('data-id');
+            $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+            $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+            $(this).addClass('active');
+            $('#'+id).addClass('active-tab').fadeIn();
+            return false;
+        });
 
     $('.icon-th-list').on('click', function(){
         $('.product__item').addClass('list');
