61d23607f1303678be82df8fefeb8565fa0879cf
UladzinirBankouski <vlad.banek@gmail.com>
2020-09-04 13:20:47 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -13,6 +13,32 @@ $(function(){
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+              breakpoint: 1900,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+              }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 1
+                }
+              },
+              {
+                breakpoint: 801,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              },
+            
+        ]
     });
    
     $(".js-range-slider").ionRangeSlider({
@@ -38,11 +64,11 @@ $(function(){
     $('.icon-th-list').on('click', function(){
         $('.product__item').addClass('list');
         $('.icon-th-list').addClass('active');
-        $('.icon-th-large').removeClass('active');
+        $('.icon-th').removeClass('active');
     });
-    $('.icon-th-large').on('click', function(){
+    $('.icon-th').on('click', function(){
         $('.product__item').removeClass('list');
-        $('.icon-th-large').addClass('active');
+        $('.icon-th').addClass('active');
         $('.icon-th-list').removeClass('active');
     });
     $('input[type="file"], select').styler();
